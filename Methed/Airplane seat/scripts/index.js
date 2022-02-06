d58cd697ac807be37b5a15473fc293f1c0d15689
Andrey <17flash02@gmail.com>
2022-02-06 22:09:52 +0600

    init

---
+++ Methed/Airplane seat/scripts/index.js
@@ -0,0 +1,24 @@
+import start from "./modules/start.js"
+import getFormPerson from "./modules/formPerson.js";
+// import start from "./modules/createElement.js"
+
+
+const init = (selectorApp, title) => {
+	const app = document.querySelector(selectorApp);
+	const { main, firstForm } = start(app, title)
+
+	firstForm.addEventListener('submit', (event) => {
+		event.preventDefault();
+
+		const forms = getFormPerson(firstForm.count.value)
+		firstForm.remove();
+
+		main.append(...forms)
+	})
+};
+
+init('.app', 'Выберите тур');
+
+
+// const arr = [1, 2, 3, 'hi', true];
+// console.log(...arr);
